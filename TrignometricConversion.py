2190fc5377a1dadf65f334113745a6ff389cac52
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2018-11-01 22:10:04 +0530

    Create TrignometricConversion.py

---
+++ TrignometricConversion.py
@@ -0,0 +1,18 @@
+#This code for trignometric and angular conversion functions
+
+import math
+
+print('The value of Sin(60 degree): ' + str(math.sin(math.radians(60))))
+print('The value of cos(pi): ' + str(math.cos(math.pi)))
+print('The value of tan(90 degree): ' + str(math.tan(math.pi/2)))
+
+print('The angle of sin(0.8660254037844386): ' + str(math.degrees(math.asin(0.8660254037844386))))
+
+
+'''
+OP :- 
+The value of Sin(60 degree): 0.866025403784
+The value of cos(pi): -1.0
+The value of tan(90 degree): 1.63312393532e+16
+The angle of sin(0.8660254037844386): 60.0
+'''
